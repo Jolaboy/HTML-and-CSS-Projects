4b2fb50a4496605cadb70d8d64cfac09296fc26c
Amadou Jarju <163463239+Jolaboy@users.noreply.github.com>
2025-01-02 20:20:03 +0000

    HTML and JavaScript upload

---
+++ Basic_HTML_and_Javascript_1/Basic_JavaScript_1.js
@@ -0,0 +1,10 @@
+
+
+
+function My_First_Function() {
+        var String = "Kiss me, I'm Gambian!";
+        var	result = String.fontcolor("green");
+        document.getElementById("Gambian").innerHTML =
+        result;
+}
+
